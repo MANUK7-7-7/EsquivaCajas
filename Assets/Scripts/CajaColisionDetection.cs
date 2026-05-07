3a3baf84707545f85517a641d46602f8f24fe839
MANUK7-7-7 <manukaplan@icloud.com>
2026-05-07 11:45:02 -0300

    movimiento de respawneo de caja

---
+++ Assets/Scripts/CajaColisionDetection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CajaColisionDetection : MonoBehaviour
+{
+    public CajaMovement cajaMovementScript;
+
+    
+    
+     void Start()
+    {
+      cajaMovementScript = GetComponent<CajaMovement>();  
+    }
+
+
+
+
+
+
+
+     void OnCollisionEnter(Collision col)
+     {
+        if(col.gameObject.CompareTag("piso")){
+           cajaMovementScript.MoveBoxToRandomPosition();
+        }
+     }
+}
--- Assets/Scripts/CajaMovement.cs
+++ Assets/Scripts/CajaMovement.cs
@@ -6,17 +6,27 @@ public class CajaMovement : MonoBehaviour
     
 {
     public float speed = 2f;
+    
+    float initialYValue;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       initialYValue = transform.position.y; 
     }
 
     // Update is called once per frame
     void Update()
     {
           transform.Translate(0,-speed * Time.deltaTime,0);
+    }
+
+    public void MoveBoxToRandomPosition()
+    {
+        Vector3 newPosition = transform.position;
+        newPosition.y = initialYValue;
+        transform.position = newPosition; 
+
     }
 }
