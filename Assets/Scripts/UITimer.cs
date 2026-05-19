bafbed45cea19dd4a697f4428ef6afbb249afb19
MANUK7-7-7 <manukaplan@icloud.com>
2026-05-19 13:33:10 -0300

    Update UITimer.cs

--- Assets/Scripts/UITimer.cs
+++ Assets/Scripts/UITimer.cs
@@ -1,21 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro; // Si usas TextMeshPro (recomendado)
+// using UnityEngine.UI; // Descomenta esta línea y comenta la de arriba si usas UI Text clásico
 
-public class UITimer : MonoBehaviour
+public class TiempoContador : MonoBehaviour
 {
-    public TextMeshProUGUI textoTiempo;
-    private float tiempo = 0f;
-    public bool juegoTerminado = false;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public TextMeshProUGUI textoTiempo; // Arrastrá el Text de la UI aquí en el Inspector
+    // public Text textoTiempo;         // Usá esta línea si usas UI Text clásico
 
-    // Update is called once per frame
+    private float tiempoTranscurrido = 0f;
+    private bool corriendo = true;
+
     void Update()
     {
-        
+        if (corriendo)
+        {
+            tiempoTranscurrido += Time.deltaTime;
+            textoTiempo.text = tiempoTranscurrido.ToString("F2") + "s";
+        }
+    }
+
+    // Llamá este método cuando el player muere para pausar el contador
+    public void PausarContador()
+    {
+        corriendo = false;
     }
 }
